b0f660c412807f49ff9431fbbc074a4877fad6dc
aleks <ivann66722@gmail.com>
2022-11-20 22:37:16 +0200

    new commit

--- build/config/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roleList = void 0;
-exports.roleList = {
-    user: 'user',
-    editor: 'editor',
-    admin: 'admin'
-};
--- build/middleware/multer.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadSingle = void 0;
-const multer_1 = __importDefault(require("multer"));
-const uploadFiles_1 = require("../config/uploadFiles");
-exports.uploadSingle = (0, multer_1.default)({ storage: uploadFiles_1.storage, fileFilter: uploadFiles_1.fileFilter }).single('avatar');
